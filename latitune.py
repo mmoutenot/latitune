b4609d1e909a8dd79e5b06defa95243346147f16
Marshall <mmoutenot@gmail.com>
2012-11-10 12:51:30 -0500

    add models

--- latitune.py
+++ latitune.py
@@ -3,9 +3,14 @@ from flask import Flask
 from flask_heroku import Heroku
 from flask.ext.sqlalchemy import SQLAlchemy
 
+# for password hashing
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app    = Flask (__name__)
 heroku = Heroku (app)
 db     = SQLAlchemy (app)
+
+# CONTROLLERS
 
 @app.route("/")
 def index():
@@ -16,3 +21,39 @@ if __name__ == "__main__":
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port)
 
+# MODEL DEFINITIONS
+class User(db.Model):
+  __tablename__ = 'user'
+
+  id      = db.Column(db.Integer, primary_key = True)
+  name    = db.Column(db.String(80))
+  email   = db.Column(db.String(120), unique  = True)
+  pw_hash = db.Column(db.String(120))
+
+  def __init__(self, name, email, password):
+    self.name = name
+    self.email = email
+    self.set_password(password)
+
+  def set_password(self, password):
+    self.pw_hash = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.pw_hash, password)
+
+class Song(db.Model):
+  __tablename__ = 'song'
+
+  id               = db.Column(db.Integer, primary_key = True)
+  artist           = db.Column(db.String(80))
+  title            = db.Column(db.String(120))
+  album            = db.Column(db.String(80))
+  provider_song_id = db.Column(db.String(200))
+  provider_key     = db.Column(db.Enum('Spotify','Youtube', name='provider_key')
+  blip             = relationship("Blip", backref="song")
+
+class Blip(db.Model):
+  __tablename__ = 'blip'
+
+  id = db.Column(db.Integer, primary_key = True)
+  song_id = db.Column(db.Integer, ForeignKey('song.id'))

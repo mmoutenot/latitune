08ff7a7a11b818b1b56ae47d85b7b2460d0e7a95
Marshall <mmoutenot@gmail.com>
2012-11-10 11:46:02 -0500

    add flask hello world

--- latitune.py
+++ latitune.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,7 @@ def hello():
   return "Hello Latitune!"
 
 if __name__ == "__main__":
-  app.run()
+  # Bind to PORT if defined, otherwise default to 5000.
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
+

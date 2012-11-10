dd913358b77e087844e219e1d5389bc3117c3ce4
Marshall <mmoutenot@gmail.com>
2012-11-10 14:35:52 -0500

    add blip routes

--- latitune.py
+++ latitune.py
@@ -53,7 +53,7 @@ def get_blip():
   if 'id' in request.args:
     blip_id = request.args['id']
     blip = Blip.query.filter_by(id=blip_id)
-    return jsonify(API_Response("OK", "", dict(blip)))
+    return jsonify(API_Response("OK", "", [dict(blip)]))
 
 @app.route("/api/blip", methods=['PUT'])
 def create_blip():

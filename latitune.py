0b7e2d9db34de5e39ce4f20cc7035ef444b8973b
Marshall <mmoutenot@gmail.com>
2012-11-10 22:01:01 -0500

    add youtube

--- latitune.py
+++ latitune.py
@@ -12,8 +12,7 @@ import gdata.youtube
 import gdata.youtube.service
 
 yt_service = gdata.youtube.service.YouTubeService()
-yt_service.developer_key =
-'AI39si4fdpqYBz4_a6E7choIqT5hIlYhbI4Ucp5eiXGDt5jzE46XM_KxWn5KtwdrAZp6WeMF9Jrzk-sXabs0R_F9T9MHZdiOYA'
+yt_service.developer_key = 'AI39si4fdpqYBz4_a6E7choIqT5hIlYhbI4Ucp5eiXGDt5jzE46XM_KxWn5KtwdrAZp6WeMF9Jrzk-sXabs0R_F9T9MHZdiOYA'
 
 app       = Flask (__name__)
 app.debug = True

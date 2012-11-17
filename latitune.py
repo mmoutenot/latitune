5da82719f8980811a9d704180a48849ccbd8a222
benweitzman <benjamin.weitzman@tufts.edu>
2012-11-17 15:21:16 -0500

    update db if on heroku

--- latitune.py
+++ latitune.py
@@ -252,6 +252,7 @@ class Blip(db.Model):
 if __name__ == "__main__":
   if len(sys.argv) == 1:
     heroku    = Heroku(app)
+    db        = SQLAlchemy (app)
   # Bind to PORT if defined, otherwise default to 5000.
   port = int(os.environ.get('PORT', 5000))
   app.run(host='0.0.0.0', port=port)

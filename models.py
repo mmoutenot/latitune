96590299740c929665299eedb1e8253646d6e0ff
Marshall <mmoutenot@gmail.com>
2013-02-24 00:47:00 -0500

    first commit

--- models.py
+++ models.py
@@ -8,6 +8,17 @@ from settings import *
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
+groups_to_users_table = Table('groups_to_users', Base.metadata,
+    Column('group_id', Integer, ForeignKey('group.id')),
+    Column('user_id',  Integer, ForeignKey('user.id'))
+)
+
+posts_to_tags_table = Table('posts_to_tags', Base.metadata,
+    Column('post_id', Integer, ForeignKey('post.id')),
+    Column('tag_id',  Integer, ForeignKey('tag.id'))
+)
+
+
 class User(db.Model):
   __tablename__ = 'user'
 
@@ -15,7 +26,7 @@ class User(db.Model):
   name    = db.Column(db.String(80), unique = True)
   email   = db.Column(db.String(120), unique = True)
   pw_hash = db.Column(db.String(120))
-  blip    = db.relationship("Blip", backref="user")
+  post    = db.relationship("Post", backref="user")
 
   def __init__(self, name, email, password):
     self.name = name
@@ -37,113 +48,87 @@ class User(db.Model):
       'email' :self.email,
     }
 
-class Song(db.Model):
-  __tablename__ = 'song'
+class Post(db.Model):
+  __tablename__ = 'post'
 
-  id               = db.Column(db.Integer, primary_key = True)
-  artist           = db.Column(db.String(80))
-  title            = db.Column(db.String(120))
-  album            = db.Column(db.String(80))
-  provider_song_id = db.Column(db.String(200))
-  provider_key     = db.Column(db.Enum('Spotify','Youtube',
-                                       name='provider_key'))
-  blip             = db.relationship("Blip", backref="song")
+  id         = db.Column(db.Integer, primary_key = True)
+  kind       = db.Column(db.String(80))
+  url        = db.Column(db.String(120))
+  timestamp = db.Column(db.DateTime, default=datetime.now)
+  group_id   = db.Column(db.Integer, db.ForeignKey('group.id'))
+  user_id    = db.Column(db.Integer, db.ForeignKey('user.id'))
+  tags       = db.relationship("Tag", secondary = posts_to_tags_table, backref = "post")
 
-  def __init__(self, artist, title, album="", provider_key="Youtube"):
-    self.artist = artist
-    self.title  = title
-    self.album  = album
-
-    query = gdata.youtube.service.YouTubeVideoQuery()
-    query.vq = title + " " + artist
-    query.orderby = 'relevance'
-    query.racy = 'include'
-    feed = yt_service.YouTubeQuery(query)
-    entry = feed.entry[0]
-    self.provider_song_id = entry.id.text.split('/')[-1]
-
-    self.provider_key = provider_key
+  def __init__(self, kind, url, group, user, tags):
+    self.kind  = kind
+    self.url   = url
+    self.group = group
+    self.user  = user
+    self.tags  = tags
 
   @property
   def serialize(self):
+    group = Group.query.filter_by(id=self.group_id).first()
+    user  = Group.query.filter_by(id=self.user_id).first()
     return {
-      'id'               : self.id,
-      'artist'           : self.artist,
-      'title'            : self.title,
-      'album'            : self.album,
-      'provider_song_id' : self.provider_song_id,
-      'provider_key'     : str(self.provider_key)
+      'id'         : self.id,
+      'kind'       : self.kind,
+      'url'        : self.url,
+      'date_added' : self.date_added,
+      'group'      : group.serialize,
+      'user'       : user.serialize,
+      'tags'       : str(self.tags)
     }
 
 
-class Blip(db.Model):
-  __tablename__ = 'blip'
+class Group(db.Model):
+  __tablename__ = 'group'
 
   id        = db.Column(db.Integer, primary_key = True)
-  song_id   = db.Column(db.Integer, db.ForeignKey('song.id'))
-  user_id   = db.Column(db.Integer, db.ForeignKey('user.id'))
-  longitude = db.Column(db.Float)
-  latitude  = db.Column(db.Float)
-  timestamp = db.Column(db.DateTime, default=datetime.now)
+  users     = db.relationship("User", secondary = groups_to_users_table, backref = "group")
+  posts     = db.relationship("Post", backref = "group")
 
-  def __init__(self, song_id, user_id, longitude, latitude):
-    self.song_id   = song_id
-    self.user_id   = user_id
-    self.longitude = longitude
-    self.latitude  = latitude
+  def __init__(self, users, posts):
+    self.users = users
+    self.posts = posts
 
   @property
   def serialize(self):
     song = Song.query.filter_by(id=self.song_id).first()
     return {
-      'id'        : self.id,
-      'song'      : song.serialize,
-      'user_id'   : self.user_id,
-      'longitude' : self.longitude,
-      'latitude'  : self.latitude,
-      'timestamp' : self.timestamp.isoformat()
+      'id' : self.id,
+      'users' : [user.serialize for user in users],
+      'posts' : [post.serialize for post in posts]
     }
 
-class Comment(db.Model):
-  __tablename__ = "comment"
+class Tag(db.Model):
+  __tablename__ = 'tag'
 
   id = db.Column(db.Integer, primary_key = True)
-  blip_id   = db.Column(db.Integer, db.ForeignKey('blip.id'))
-  user_id   = db.Column(db.Integer, db.ForeignKey('user.id'))
-  comment   = db.Column(db.Text)
-  timestamp = db.Column(db.DateTime, default=datetime.now)
+  name = db.Column(db.String(20))
 
-  def __init__(self, user_id,blip_id,comment):
-    self.user_id = user_id
-    self.blip_id = blip_id
-    self.comment = comment
+# class Comment(db.Model):
+#   __tablename__ = "comment"
+#
+#   id        = db.Column(db.Integer, primary_key = True)
+#   blip_id   = db.Column(db.Integer, db.ForeignKey('blip.id'))
+#   user_id   = db.Column(db.Integer, db.ForeignKey('user.id'))
+#   comment   = db.Column(db.Text)
+#   timestamp = db.Column(db.DateTime, default=datetime.now)
+#
+#   def __init__(self, user_id,blip_id,comment):
+#     self.user_id = user_id
+#     self.blip_id = blip_id
+#     self.comment = comment
+#
+#   @property
+#   def serialize(self):
+#     blip = Blip.query.filter_by(id=self.blip_id).first()
+#     return {
+#       'id'       : self.id,
+#       'blip'     : blip.serialize,
+#       'comment'  : self.comment,
+#       'user_id'  : self.user_id,
+#       'timestamp': self.timestamp.isoformat()
+#     }
 
-  @property
-  def serialize(self):
-    blip = Blip.query.filter_by(id=self.blip_id).first()
-    return {
-      'id'       : self.id,
-      'blip'     : blip.serialize,
-      'comment'  : self.comment,
-      'user_id'  : self.user_id,
-      'timestamp': self.timestamp.isoformat()
-    }
-
-class Favorite(db.Model):
-  __tablename__ = "favorite"
-
-  id      = db.Column(db.Integer, primary_key = True)
-  user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-  blip_id = db.Column(db.Integer, db.ForeignKey('blip.id'))
-
-  def __init__(self, user_id, blip_id):
-    self.user_id = user_id
-    self.blip_id = blip_id
-
-  @property 
-  def serialize(self):
-    return {
-      'id'     : self.id,
-      'user_id': self.user_id,
-      'blip_id': self.blip_id
-    }
